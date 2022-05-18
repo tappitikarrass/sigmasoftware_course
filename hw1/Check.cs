ee32aa777795a404ea0c28fa493c215c67f12582
Andrii Lytvyn <lytvyn349@gmail.com>
2022-05-19 01:38:06 +0300

    Upload Homework 1

---
+++ hw1/Check.cs
@@ -0,0 +1,22 @@
+﻿namespace hw1
+{
+    public class Check
+    {
+        public static void PrintBuyInfo(Buy buy)
+        {
+            foreach(var entry in buy.ProductsDict)
+            {
+                PrintProductInfo(entry.Key);
+                Console.WriteLine("\nquantity: {0}\n", entry.Value);
+            }
+            Console.WriteLine("total quantity: {0}\ntotal price: {1}\ntotal weight: {2}",
+                buy.TotalQuantity, buy.TotalPrice, buy.TotalWeight);
+        }
+
+        public static void PrintProductInfo(Product product)
+        {
+            Console.Write("name: {0}\nprice: {1}$\nweight: {2}kg",
+                product.Name, product.Price, product.Weight);
+        }
+    }
+}
--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,7 +1,19 @@
-﻿public class Program
+﻿namespace hw1
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello hw1");
+        public static void Main(string[] args)
+        {
+            var Product1 = new Product("Potato", 3, 5);
+            var Product2 = new Product("Tomato", 5, 1);
+            var Product3 = new Product("Apple", 4, 2);
+
+            var Buy1 = new Buy();
+            Buy1.AddProduct(Product1, 3);
+            Buy1.AddProduct(Product3, 5);
+            Buy1.AddProduct(Product1, 7);
+
+            Check.PrintBuyInfo(Buy1);
+        }
     }
 }
